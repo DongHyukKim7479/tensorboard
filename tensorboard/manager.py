6e3fbd51fa35733241f112112309fa5acdbf7839
William Chargin <wchargin@gmail.com>
2019-01-31 17:16:34 -0800

    manager: add draft of `manager.start`
    Test Plan:
    
        $ bazel build //tensorboard
        $ export PATH="$(readlink -e ./bazel-bin/tensorboard):$PATH"
        >>> from tensorboard import manager
        >>> manager.start(["--logdir", "~/tensorboard_data/", "--port", "0"])
        StartLaunched(info=TensorboardInfo(version=u'1.13.0a0', start_time=1548983996, pid=159880, port=37345, path_prefix=u'', logdir=u'~/tensorboard_data/', db=u'', cache_key=u'<snip>'))
        >>> manager.start(["--logdir", "~/tensorboard_data/", "--port", "0"])
        StartReused(info=TensorboardInfo(version=u'1.13.0a0', start_time=1548983996, pid=159880, port=37345, path_prefix=u'', logdir=u'~/tensorboard_data/', db=u'', cache_key=u'<snip>'))
        >>> manager.start(["--no_logdir_for_you"])
        StartFailed(exit_code=2, stdout='', stderr='usage: <snip> error: unrecognized arguments: --no_logdir_for_you\n')
        >>> import os, signal
        >>> os.kill(159880, signal.SIGTERM)
        >>> manager.start(["--logdir", "~/tensorboard_data/", "--port", "0"])
        StartLaunched(info=TensorboardInfo(version=u'1.13.0a0', start_time=1548984112, pid=160354, port=45307, path_prefix=u'', logdir=u'~/tensorboard_data/', db=u'', cache_key=u'<snip>'))
        >>> exit()
        $ curl localhost:45307/data/logdir
        {"logdir": "~/tensorboard_data/"}
        $ kill 160354
    
    wchargin-branch: manager-start

--- tensorboard/manager.py
+++ tensorboard/manager.py
@@ -25,6 +25,7 @@ import json
 import os
 import subprocess
 import tempfile
+import time
 
 from tensorboard.util import tb_logging
 
@@ -147,3 +148,81 @@ def get_all():
     else:
       results.append(info)
   return results
+
+
+def _find_matching_instance(cache_key):
+  infos = get_all()
+  candidates = [info for info in infos if info.cache_key == cache_key]
+  for candidate in sorted(candidates, key=lambda x: x.port):
+    # TODO(@wchargin): Check here that the provided port is still live?
+    return candidate
+  return None
+
+
+
+# Indicates that a call to `start` was compatible with an existing
+# TensorBoard process, which can be reused according to the provided
+# info.
+StartReused = collections.namedtuple("StartReused", ("info",))
+
+# Indicates that a call to `start` successfully launched a new
+# TensorBoard process, which is available with the provided info.
+StartLaunched = collections.namedtuple("StartLaunched", ("info",))
+
+# Indicates that a call to `start` tried to launch a new TensorBoard
+# instance, but the subprocess exited with the given exit code and
+# output streams.
+StartFailed = collections.namedtuple(
+    "StartFailed",
+    (
+        "exit_code",  # int, as `Popen.returncode` (negative for signal)
+        "stdout",
+        "stderr",
+    ),
+)
+
+# Indicates that a call to `start` launched a TensorBoard process, but
+# that process neither exited nor wrote its info file within the allowed
+# timeout period. The process may still be running under the included
+# pid.
+StartTimedOut = collections.namedtuple("StartTimedOut", ("pid",))
+
+
+def start(arguments, timeout_seconds=10):
+  match = _find_matching_instance(cache_key(os.getcwd(), arguments))
+  if match:
+    return StartReused(info=match)
+
+  (stdout_fd, stdout_path) = tempfile.mkstemp(prefix="tensorboard-stdout-")
+  (stderr_fd, stderr_path) = tempfile.mkstemp(prefix="tensorboard-stderr-")
+  start_time = time.time()
+  try:
+    p = subprocess.Popen(
+        ["tensorboard"] + arguments,
+        stdout=stdout_fd,
+        stderr=stderr_fd,
+    )
+  finally:
+    os.close(stdout_fd)
+    os.close(stderr_fd)
+
+  poll_interval_seconds = 0.5
+  end_time = start_time + timeout_seconds
+  while time.time() < end_time:
+    time.sleep(poll_interval_seconds)
+    subprocess_result = p.poll()
+    if subprocess_result is not None:
+      with open(stdout_path) as stdout_infile:
+        stdout = stdout_infile.read()
+      with open(stderr_path) as stderr_infile:
+        stderr = stderr_infile.read()
+      return StartFailed(
+          exit_code=subprocess_result,
+          stdout=stdout,
+          stderr=stderr,
+      )
+    for info in get_all():
+      if info.pid == p.pid and info.start_time >= start_time:
+        return StartLaunched(info=info)
+  else:
+    return StartTimedOut(pid=p.pid)
